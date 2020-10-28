cb4d09eb799fc3597b579bec8cbdcdd2ae67e86d
Zainjer <arainareeb@gmail.com>
2020-10-28 22:34:58 +0500

    Code cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,29 +2,15 @@ package com.company;
 import org.opencv.core.*;
 import org.opencv.core.Point;
 import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
-import org.opencv.videoio.Videoio;
-
-import java.awt.*;
-import java.io.IOException;
-import java.util.Timer;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
 
-    private CascadeClassifier faceCascade;
-    private VideoCapture capture;
-    private int absoluteFaceSize;
-    private Timer timer;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         CascadeClassifier faceCascade = new CascadeClassifier();
         CascadeClassifier eyeCascade = new CascadeClassifier();
@@ -82,7 +68,6 @@ public class Main {
                     //if(eyeRects.length>1) // For eye draw on all faces
                     {
                         for (Rect e:eyeRects) {
-                            //209, 195, 0
                             Point tl = new Point(r.tl().x+e.tl().x,r.tl().y+e.tl().y);
                             Point br = new Point(r.tl().x+e.br().x,r.tl().y+e.br().y);
 
@@ -112,7 +97,7 @@ public class Main {
 
                     //MOUTH DETECTION CODE STARTS HERE
                     MatOfRect mouth = new MatOfRect();
-                    mouthCascade.detectMultiScale(face,mouth,1.5,4,0);
+                    mouthCascade.detectMultiScale(face,mouth,1.5,5,0);
                     Rect[] mouthRects = mouth.toArray();
 
                     if(mouthRects.length==1)
@@ -122,34 +107,14 @@ public class Main {
                             Point tl = new Point(r.tl().x+m.tl().x,r.tl().y+m.tl().y);
                             Point br = new Point(r.tl().x+m.br().x,r.tl().y+m.br().y);
 
-                            Imgproc.rectangle(frame,tl,br, new Scalar(100, 100, 181),2);
+                            Imgproc.rectangle(frame,tl,br, new Scalar(50, 100, 255),2);
                         }
                     }
                     //MOUTH DETECTION CODE ENDS HERE
-
-
                 }
             }
-//            MatOfRect eyes = new MatOfRect();
-//            eyeCascade.detectMultiScale(face,eyes,1.1,5,0);
-//            Rect[] eyeRects = eyes.toArray();
-//            if(eyeRects.length<=2 && eyeRects.length>0 ){
-//                for (Rect e:eyeRects) {
-//                    //209, 195, 0
-//                    Point tl = new Point(faceRects[0].tl().x+e.tl().x,faceRects[0].tl().y+e.tl().y);
-//                    Point br = new Point(faceRects[0].tl().x+e.br().x,faceRects[0].tl().y+e.br().y);
-//
-//                    Imgproc.rectangle(frame,tl,br, new Scalar(123, 189, 0),2);
-//                }
-//            }
-
-                //Imgproc.rectangle(frame, faceRects[0], new Scalar(209, 195, 0),2);
             HighGui.imshow("Frame", frame);
-
             if (HighGui.waitKey(30)>=0) break;
-
         }
-
     }
-
 }
